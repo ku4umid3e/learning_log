5bb13c79850a95a3ea9bb5163412869c415e1e65
ku4umid3e <kuchumid3e@gmail.com>
2021-07-25 23:58:02 +0300

    Added a page with topics and a review page of a separate topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,8 +3,12 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'learning_logs'
 urlpatterns = [
-        #Home page
+        # Home page
         path('', views.index, name='index'),
+        # Output all topics.
+        path('topics', views.topics, name='topics'),
+        # Page with detailed information on a separate topic.
+        path(r'^topics/(?P<topic_id>\d+)$', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Home page of the Lerning Log application"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Displays a list of topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Display one topic and all its records."""
+    topic = Topic.objects.get(id=topic_id)
+    entries =  topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
