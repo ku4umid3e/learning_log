71444727296307dc1e2949f6675457ae7710ff44
ku4umid3e <kuchumid3e@gmail.com>
2021-08-10 00:23:21 +0300

    Added forms

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from .models import Topic
+from .forms import TopicForms
 
 # Create your views here.
 def index(request):
@@ -18,3 +22,17 @@ def topic(request, topic_id):
     entries =  topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Determines a new topic."""
+    if request.method != 'POST':
+        # The data was not sent; Created empty form.
+        form =TopicForm()
+    else:
+        # POST data sent; process data.
+        from = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form' : form}
+    return render(request, 'learning_logs/new_topic.html', context)
