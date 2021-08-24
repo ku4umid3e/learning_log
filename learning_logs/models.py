4b0c4ac26ae7a6f54084c191805302f4d6e6ff20
ku4umid3e <kuchumid3e@gmail.com>
2021-08-24 23:01:56 +0300

    Added new_entry.html and brought the project to a working state

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Topic(models.Model):
     """Topic that the user is studying."""
     text = models.CharField(max_length=200)
-    date_added =  models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Returns a string representation of the model."""
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
-    """Home page of the Lerning Log application"""
+    """Home page of the Learning Log application"""
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
@@ -36,3 +36,22 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form' : form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a specific topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #No data was sent; an empty forms created
+        form = EntryForm()
+
+    else:
+        #POST data has been sent; process the data.
+        form=EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic' : topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
