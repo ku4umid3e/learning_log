529449b70357974a3b8d71d195fbbee0bceadd45
ku4umid3e <kuchumid3e@gmail.com>
2021-08-24 22:58:11 +0300

    Changed URL to regular expressions. It didnt work without it.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,6 +1,6 @@
-'''Defines URL schemes for learning_logs'''
+"""Defines URL schemes for learning_logs"""
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'learning_logs'
@@ -8,11 +8,11 @@ urlpatterns = [
         # Home page
         path('', views.index, name='index'),
         # Output all topics.
-        path('topics', views.topics, name='topics'),
+        re_path(r'^topics/$', views.topics, name='topics'),
         # Page with detailed information on a separate topic.
-        path('topic', views.topic, name='topic'),
+        re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
         # Page to add a new topic.
-        path('new_topic', views.new_topic, name='new_topic'),
+        re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
         # Page to add a new entry.
-        path('new_entry', views.new_entry, name='new_entry'),
+        re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
