4035ed24083057ca4c6488febd91cacb2e8ddcb7
ku4umid3e <kuchumid3e@gmail.com>
2021-08-26 23:15:26 +0300

    Added the ability to edit entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
         re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
         # Page to add a new entry.
         re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+        # Page to edit entry
+        re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -55,3 +55,21 @@ def new_entry(request, topic_id):
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
     context = {'topic' : topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+    
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Original request; the form is filled with the data of the current record.
+        form = EntryForm(instance=entry)
+    else:
+        #POST data has been sent; process the data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
