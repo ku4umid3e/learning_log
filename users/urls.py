c1016441b4c129c9f1177f0d09f1fabeefe3aabe
ku4umid3e <kuchumid3e@gmail.com>
2021-11-30 11:48:55 +0300

    Added a form of registration.

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
+
 app_name = 'users'
 urlpatterns = [
             #Enable defaullt authorization URL.
             path('', include('django.contrib.auth.urls')),
+            #Register page
+            path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    """Registers a new user"""
+    if request.method != 'POST':
+        #Displays an empty registration form.
+        form = UserCreationForm()
+    else:
+        #Processing filled form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #Entry and redirection to the homepage.
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    #
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
