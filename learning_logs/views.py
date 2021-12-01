6b834149d7cfedf8710e5317030b05488f74547c
ku4umid3e <kuchumid3e@gmail.com>
2021-12-01 12:03:14 +0300

    Make registration page. Each registered user can create and edit topics. User cannot see and edit other users topics. Other users also can not see and edit his topics.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,75 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
+
 # Create your views here.
 def index(request):
     """Home page of the Learning Log application"""
     return render(request, 'learning_logs/index.html')
 
+
+@login_required
 def topics(request):
     """ Displays a list of topics."""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics' : topics}
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
+@login_required
 def topic(request, topic_id):
     """ Display one topic and all its records."""
     topic = Topic.objects.get(id=topic_id)
-    entries =  topic.entry_set.order_by('-date_added')
+    # Checking that page belongsto the current user.
+    if topic.owner != request.user:
+        raise Http404
+    entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
+@login_required
 def new_topic(request):
     """Determines a new topic."""
     if request.method != 'POST':
         # The data was not sent; Created empty form.
-        form =TopicForm()
+        form = TopicForm()
     else:
         # POST data sent; process data.
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a specific topic."""
     topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
-        #No data was sent; an empty forms created
+        # No data was sent; an empty forms created
         form = EntryForm()
 
     else:
-        #POST data has been sent; process the data.
-        form=EntryForm(data=request.POST)
+        # POST data has been sent; process the data.
+        form = EntryForm(data=request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
-    context = {'topic' : topic, 'form': form}
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
-    
+
+
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
-        #Original request; the form is filled with the data of the current record.
+        # Original request; the form is filled with
+        # the data of the current record.
         form = EntryForm(instance=entry)
     else:
-        #POST data has been sent; process the data.
+        # POST data has been sent; process the data.
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
-    
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
