4cadaeeb1a3a76239ae4efc69eb3f6847bd20a2d
Dmitrii Ivanov <dmitrii.ivanov.m@outlook.com>
2022-10-19 15:43:44 +0300

    corrected my mistake with pages 404 and 500

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jmge6@azmv+5b7r^p7fdw@p_o4wf=*7rs9^+e-_s-p0!w7%7rs'
+SECRET_KEY = 'django-insecure-jmge6v+5b7r^p7fdw@p_o4wf=*7rs9^+e-_s-p0!w7rs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -61,6 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            BASE_DIR / 'templates',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
